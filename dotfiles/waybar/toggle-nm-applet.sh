b0bad6ace7b186758346dbbb3475ab1d52127faf
Kou-ding <fotispapadak@gmail.com>
2025-07-14 22:03:48 +0300

    new waybar

--- dotfiles/waybar/toggle-nm-applet.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if pgrep -x "nm-applet" > /dev/null; then
-    # If nm-applet is running, kill it
-    pkill -x "nm-applet"
-else
-    # If not running, launch it
-    nm-applet --indicator &
-fi
